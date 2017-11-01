9cdc8667af325521e7a4236f772b247d6c9f794c
James Gomatos <goarmygomatos@gmail.com>
2017-11-01 10:05:34 -0400

    added engine and mechanic list queries

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, abort, flash
 from flask_login import login_required, current_user
 from . import main
 from .. import db
-from ..models import User, Aircraft
+from ..models import User, Aircraft, Engine, Mechanic
 from sqlalchemy.sql import select
 
 '''
@@ -26,3 +26,19 @@ render the aircrafts when button pressed in the mechanic menu
 def mechanic_get_aircraft():
     data = Aircraft.query.all()
     return render_template('mechanic/aircraft.html', data=data)
+
+'''
+render list of engines when button pressed in the mechanic menu
+'''
+@main.route('/mechanic/engine')
+def mechanic_get_engine():
+    data = Engine.query.all()
+    return render_template('mechanic/engine.html', data=data)
+
+'''
+render list of mechanics when button pressed in the mechanic menu
+'''
+@main.route('/mechanic/list')
+def mechanic_get_list():
+    data = Mechanic.query.all()
+    return render_template('mechanic/list.html', data=data)
